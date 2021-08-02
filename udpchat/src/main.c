8fe2ae2d6c248193dc041814f7b6f14464cd76ef
Daniel Florescu <df@z5.lt>
2021-08-02 09:51:22 +0200

    Rename logging to print & continue main.c

--- udpchat/src/main.c
+++ udpchat/src/main.c
@@ -6,6 +6,9 @@
 #include <stdbool.h>
 #include <poll.h>
 #include <pthread.h>
+#include <string.h>
+#include <errno.h>
+#include <unistd.h>
 
 #include <netdb.h>
 
@@ -18,29 +21,39 @@
 /* == Compile time options == */
 /* = Logging = */
 /* Controls wether the log* functions print anything. */
-#ifndef LOG_DEBUG
-#define LOG_DEBUG (1)
+#ifndef PRINT_DEBUG
+#define PRINT_DEBUG (1)
 #endif
-#ifndef LOG_INFO
-#define LOG_INFO (1)
+#ifndef PRINT_INFO
+#define PRINT_INFO (1)
 #endif
-#ifndef LOG_WARN
-#define LOG_WARN (1)
+#ifndef PRINT_WARN
+#define PRINT_WARN (1)
 #endif
-#ifndef LOG_ERROR
-#define LOG_ERROR (1)
+#ifndef PRINT_ERROR
+#define PRINT_ERROR (1)
 #endif
-#include "util/logging.h"
+#define PRINT_WRITE_MUTEX 1
+#include "util/print.h"
 /* = net =*/
 #ifndef MAX_BIND_COUNT
 /* Max count of interfaces to listen on. Normally only two are used. */
 #define MAX_BIND_COUNT (10)
+#endif
+#ifndef MAX_MSG_SIZE
+/* Max size of message that can be received. */
+#define MAX_MSG_SIZE (2048)
 #endif
 
 /* == Globals == */
 static char *progname = "";
 
 /* == Static functions == */
+static void *master_func(void *args);
+struct master_func_args {
+	int *sfd_arr;
+	size_t sfd_arr_len;
+};
 
 int main(int argc, char *argv[])
 {
@@ -52,7 +65,12 @@ int main(int argc, char *argv[])
 	/* The master thread will handle all IO while main thread will sleep.*/
 	pthread_t master_thread = 0;
 	/* Info about sockets that we will bind to. */
-	struct addrinfo *bindinfo = NULL;
+	struct addrinfo *addr = NULL;
+	/* Array of bound sockets. */
+	int sfd_arr[MAX_BIND_COUNT] = {0};
+	size_t sfd_arr_len = 0;
+	/* Args to master_func. */
+	struct master_func_args master_args = {0};
 
 	/* Set program name. */
 	progname = argv[0];
@@ -67,27 +85,115 @@ int main(int argc, char *argv[])
 		port = argv[1];
 		for (int n = 2; n < argc; ++n) {
 			if (bind_ips_len >= MAX_BIND_COUNT - 1) {
-				log_error("Too many interfaces provided");
+				perror("Too many interfaces provided");
 				goto args_err;
 			}
 			bind_ips[bind_ips_len++] = argv[n];
 		}
 	} else {
-		log_error("Not enough arguments");
+		perror("Not enough arguments");
 		goto args_err;
 	}
 
+	/* Create address info from provided infomation. */
 	ret = create_addrinfo(port, (const char **)bind_ips, bind_ips_len,
-	    &bindinfo);
+	    &addr);
 	if (ret != 0) {
-		log_error("create_addrinfo: %s", gai_strerror(ret));
+		perror("create_addrinfo: %s", gai_strerror(ret));
 		goto addrinfo_err;
 	}
 
+	/* Bind each node in addrinfo into sfd_arr. */
+	for (struct addrinfo *ca = addr; ca != NULL; ca = ca->ai_next) {
+		int sfd = socket(ca->ai_family, ca->ai_socktype,
+		    ca->ai_protocol);
+		if (sfd == -1) {
+			pwarn("socket: skipping '%s': %s",
+			    addr2str(ca->ai_family, ca->ai_addr),
+			    strerror(errno));
+			continue;
+		}
+		pdebug("socket: Created '%s'",
+		    addr2str(ca->ai_family, ca->ai_addr));
 
-	freeaddrinfo(bindinfo);
+#ifdef __linux__
+		/* Disable dual stack on Linux. */
+		if (ca->ai_family == AF_INET6) {
+			int use_v6only = true;
+			ret = setsockopt(sfd, IPPROTO_IPV6, IPV6_V6ONLY,
+			    &use_v6only, sizeof(use_v6only));
+			if (ret != 0) {
+				pwarn("Failed to disable dualstack (linux) '%s': %s",
+				    addr2str(ca->ai_family, ca->ai_addr),
+				    strerror(errno));
+				close(sfd);
+				continue;
+			}
+		}
+#endif
+		/* Bind socket. */
+		ret = bind(sfd, ca->ai_addr, ca->ai_addrlen);
+		if (ret != 0) {
+			pwarn("Failed to bind '%s': %s",
+			    addr2str(ca->ai_family, ca->ai_addr),
+			    strerror(errno));
+			close(sfd);
+			continue;
+		}
+		pdebug("Bound '%s' to %i", addr2str(ca->ai_family, ca->ai_addr),
+		    sfd);
+
+		/* Add socket to array of bound sockets. */
+		if (sfd_arr_len >= MAX_BIND_COUNT) {
+			pwarn("Reached socket limit (%zu) drop %s", sfd_arr_len,
+			    addr2str(ca->ai_family, ca->ai_addr));
+			close(sfd);
+			break;
+		}
+		sfd_arr[sfd_arr_len++] = sfd;
+	}
+	if (sfd_arr_len == 0) {
+		perror("No sockets creating, aborting");
+		goto socket_err;
+	}
+
+	/* Start master thread. */
+	master_args.sfd_arr = sfd_arr;
+	master_args.sfd_arr_len = sfd_arr_len;
+	ret = pthread_create(&master_thread, NULL, master_func, &master_args);
+	if (ret != 0) {
+		perror("Failed to create master thread: %s", strerror(ret));
+		goto pthread_err;
+	}
+
+	pthread_join(master_thread, NULL);
+pthread_err:
+	for (size_t n = 0; n < sfd_arr_len; ++n) {
+		close(sfd_arr[n]);
+	}
+socket_err:
+	freeaddrinfo(addr);
 addrinfo_err:
 args_err:
 	return status;
 }
 
+
+static void *master_func(void *args0) {
+	struct master_func_args *args = (struct master_func_args *)args0;
+	struct pollfd poll_arr[MAX_BIND_COUNT] = {0};
+	char buffer[MAX_MSG_SIZE] = {0};
+
+	/* Fill poll array. */
+	for (size_t n = 0; n < args->sfd_arr_len && n < MAX_BIND_COUNT; ++n) {
+		poll_arr[n].fd = args->sfd_arr[n];
+		poll_arr[n].events = POLLIN | POLLPRI;
+		/* The rest is already 0. */
+	}
+	for (size_t n = args->sfd_arr_len; n < MAX_BIND_COUNT; ++n) {
+		poll_arr[n].fd = -1; /* poll **will** ignore under 0. */
+		/* The rest is already 0. */
+	}
+
+	return NULL;
+}
---
+++ udpchat/src/util/print.h
@@ -0,0 +1,142 @@
+#ifndef UTIL_PRINT_H
+#define UTIL_PRINT_H
+
+#include <stdio.h>
+
+/* if you want to change output */
+/* printf will be used for DEBUG & INFO. */
+#ifndef print_printf
+#define print_printf(...) printf(__VA_ARGS__)
+#endif
+/* eprintf will be used for WARN & ERROR. */
+#ifndef print_eprintf
+#define print_eprintf(...) fprintf(stderr, __VA_ARGS__)
+#endif
+/* if enabled */
+#ifndef PRINT_DEBUG
+#define PRINT_DEBUG (0)
+#endif
+#ifndef PRINT_INFO
+#define PRINT_INFO (0)
+#endif
+#ifndef PRINT_WARN
+#define PRINT_WARN (0)
+#endif
+#ifndef PRINT_ERROR
+#define PRINT_ERROR (0)
+#endif
+/* prefixes, here the colons are aligned. */
+#ifndef PRINT_DEBUG_PREFIX
+#define PRINT_DEBUG_PREFIX "DEBUG: "
+#endif
+#ifndef PRINT_INFO_PREFIX
+#define PRINT_INFO_PREFIX  "INFO : "
+#endif
+#ifndef PRINT_WARN_PREFIX
+#define PRINT_WARN_PREFIX  "WARN : "
+#endif
+#ifndef PRINT_ERROR_PREFIX
+#define PRINT_ERROR_PREFIX "ERROR: "
+#endif
+/* if using write mutex */
+#ifndef PRINT_WRITE_MUTEX
+#define PRINT_WRITE_MUTEX 0
+#endif
+#if PRINT_WRITE_MUTEX == 1
+#include <pthread.h>
+pthread_mutex_t _print_write_mutex = PTHREAD_MUTEX_INITIALIZER;
+#endif
+
+/* Long print macro definitions */
+#if PRINT_DEBUG == 1
+#define print_debug(...) {\
+	pthread_mutex_lock(&_print_write_mutex);\
+	print_printf(PRINT_DEBUG_PREFIX);\
+	print_printf(__VA_ARGS__);\
+	print_printf("\n");\
+	pthread_mutex_unlock(&_print_write_mutex);\
+}
+#else
+#define print_debug(...)
+#endif
+#if PRINT_INFO == 1
+#define print_info(...) {\
+	pthread_mutex_lock(&_print_write_mutex);\
+	print_printf(PRINT_INFO_PREFIX);\
+	print_printf(__VA_ARGS__);\
+	print_printf("\n");\
+	pthread_mutex_unlock(&_print_write_mutex);\
+}
+#else
+#define print_info(...)
+#endif
+#if PRINT_WARN == 1
+#define print_warn(...) {\
+	pthread_mutex_lock(&_print_write_mutex);\
+	print_eprintf(PRINT_WARN_PREFIX);\
+	print_eprintf(__VA_ARGS__);\
+	print_eprintf("\n");\
+	pthread_mutex_unlock(&_print_write_mutex);\
+}
+#else
+#define print_warn(...)
+#endif
+#if PRINT_ERROR == 1
+#define print_error(...) {\
+	pthread_mutex_lock(&_print_write_mutex);\
+	print_eprintf(PRINT_ERROR_PREFIX);\
+	print_eprintf(__VA_ARGS__);\
+	print_eprintf("\n");\
+	pthread_mutex_unlock(&_print_write_mutex);\
+}
+#else
+#define print_error(...)
+#endif
+
+/* Short definitions. */
+#if PRINT_DEBUG == 1
+#define pdebug(...) {\
+	pthread_mutex_lock(&_print_write_mutex);\
+	print_printf(PRINT_DEBUG_PREFIX);\
+	print_printf(__VA_ARGS__);\
+	print_printf("\n");\
+	pthread_mutex_unlock(&_print_write_mutex);\
+}
+#else
+#define pdebug(...)
+#endif
+#if PRINT_INFO == 1
+#define pinfo(...) {\
+	pthread_mutex_lock(&_print_write_mutex);\
+	print_printf(PRINT_DEBUG_PREFIX);\
+	print_printf(__VA_ARGS__);\
+	print_printf("\n");\
+	pthread_mutex_unlock(&_print_write_mutex);\
+}
+#else
+#define pinfo(...)
+#endif
+#if PRINT_WARN == 1
+#define pwarn(...) {\
+	pthread_mutex_lock(&_print_write_mutex);\
+	print_eprintf(PRINT_WARN_PREFIX);\
+	print_eprintf(__VA_ARGS__);\
+	print_eprintf("\n");\
+	pthread_mutex_unlock(&_print_write_mutex);\
+}
+#else
+#define pwarn(...)
+#endif
+#if PRINT_ERROR == 1
+#define perror(...) {\
+	pthread_mutex_lock(&_print_write_mutex);\
+	print_eprintf(PRINT_ERROR_PREFIX);\
+	print_eprintf(__VA_ARGS__);\
+	print_eprintf("\n");\
+	pthread_mutex_unlock(&_print_write_mutex);\
+}
+#else
+#define perror(...)
+#endif
+
+#endif
